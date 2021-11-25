a1a4f2c4bcda77d2b9b2df6747ced940fce4665a
Kira Kowalski <antivianwaitress@gmx.de>
2021-11-25 08:14:29 +0100

    Abschnitt 22 EJS erl.

---
+++ todolist-v1/date.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+const today = new Date();
+
+exports.getDate = function() {
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  return today.toLocaleDateString("en-US", options);
+};
+
+exports.getDay = function() {
+  const options = {
+    weekday: "long",
+  };
+  return today.toLocaleDateString("en-US", options);
+};
