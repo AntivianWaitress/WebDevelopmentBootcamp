b9bc5b56ad11ad3299d1165fda70a0c43ec9cc5d
Kira Kowalski <antivianwaitress@gmx.de>
2021-11-29 21:14:09 +0100

    Stand 318

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -34,20 +35,24 @@ app.get("/compose", function(req, res){
 });
 
 app.get("/posts/:title", function(req, res){
+  let url = lodash.lowerCase(req.params.title);
   posts.forEach((post) => {
-    if(req.params.title == post.title){
-      console.log("Match found!");
+    let postTitle = lodash.lowerCase(post.title);
+    if(url == postTitle){
+      res.render("post", {title: post.title, text: post.text});
     }else{
-      console.log(req.params.title);
-      console.log(post.title);
+      console.log(url);
+      console.log(postTitle);
     }
   })
 });
 
 app.post("/compose", function(req, res){
+  let url = lodash.lowerCase(req.body.inputTitle);
+  url = url.replace(/ /g, "-");
   let newPost = {
     title: req.body.inputTitle,
-    text: req.body.inputPost
+    text: req.body.inputPost,
   };
   posts.push(newPost);
   res.redirect("/");
